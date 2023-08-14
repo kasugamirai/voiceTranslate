c868d12e418881e5815fea9218daf734b234fc13
xy <xyzmhx@gmail.com>
2023-08-15 03:06:17 +0900

    continue translate

--- content.js
+++ content.js
@@ -1,17 +1,32 @@
 const recognition = new webkitSpeechRecognition();
 recognition.lang = 'ja-JP';
+recognition.continuous = true;  // 让识别器持续运行
 
 recognition.onresult = function(event) {
-    const recognizedText = event.results[0][0].transcript;
-    chrome.runtime.sendMessage({type: "recognitionResult", data: recognizedText});
+    for(let i = event.resultIndex; i < event.results.length; i++) {
+        if (event.results[i].isFinal) {
+            const recognizedText = event.results[i][0].transcript;
+            chrome.runtime.sendMessage({type: "recognitionResult", data: recognizedText});
+        }
+    }
 };
 
 recognition.onerror = function(event) {
     chrome.runtime.sendMessage({type: "recognitionError", data: event.error});
 };
 
+recognition.onend = function() {
+    if (shouldRestart) recognition.start();  // 在识别结束后立刻重启
+};
+
+let shouldRestart = false;
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if(request.action === "startRecognition") {
+        shouldRestart = true;
         recognition.start();
+    } else if(request.action === "stopRecognition") {
+        shouldRestart = false;
+        recognition.stop();
     }
 });
--- popup.js
+++ popup.js
@@ -1,11 +1,18 @@
+let isRecording = false;
+
 document.getElementById("recordBtn").addEventListener("click", function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         const activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, {action: "startRecognition"});
+        isRecording = !isRecording;  // 切换录音状态
+        const action = isRecording ? "startRecognition" : "stopRecognition";
+        chrome.tabs.sendMessage(activeTab.id, {action: action});
+
+        // 更新按钮文本
+        document.getElementById("recordBtn").textContent = isRecording ? 'Stop Recording' : 'Record Voice';
     });
 });
 
-chrome.runtime.onMessage.addEventListener(function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if(request.type === "recognitionResult") {
         document.getElementById("recognizedText").innerText = request.data;
     } else if(request.type === "recognitionError") {
